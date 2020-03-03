8ec0a460f552a2b24231bf7700a3c8aa608e2c0a
Oliver Midbrink <oliver@Olivers-MBP.lan>
2020-03-03 21:09:15 +0100

    Added functionality that outputs a csv file with predictions!

--- live.py
+++ live.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 from data_tools import trim_zeros
 
 
-	# --- Requirements for data ---
+	# --- Requirements for data ---		###This file gets the latest X market days and makes predictions for the symbols loaded around line 38 ### 
 hist_time_steps = 90
 
 today = datetime.date(datetime.now())
@@ -50,18 +50,24 @@ latest_df.columns = latest_df.columns.swaplevel(0, 1)
 variables = latest_df.columns.get_level_values(1).unique()
 symbols = latest_df.columns.get_level_values(0).unique()
 
+
 print('Normalizing data...')
+
 n_df = latest_df.copy()
 n_df = n_df.fillna(method='ffill')
 n_df = n_df.fillna(0)
+
 for sym in symbols:	#iterate through stocks and normalize data for that stock
 	v_df = n_df[sym]
 	nv_df=(v_df-v_df.min())/(v_df.max()-v_df.min())
 	n_df[sym] = nv_df
+
 print('Done.')
+
 
 latest_n_df = n_df[-hist_time_steps:]	# Hist time steps long data of 500 swedish stocks
 print('Latest row: {}'.format(latest_n_df[-1:]))
+
 
 # --- Find good stocks to buy ---
 def disp_pred(prev_price, pred, title, show_now=True):
@@ -71,6 +77,7 @@ def disp_pred(prev_price, pred, title, show_now=True):
 	ax.plot(range(hist_time_steps + len(pred)), np.append([None] * hist_time_steps, pred))
 	if show_now:
 		plt.show()
+
 
 def disp_stock(sym):
 	start_hist = n_df[-hist_time_steps:-hist_time_steps+1].index.date
@@ -95,23 +102,58 @@ def disp_stock(sym):
 
 	plt.show()
 
+
+
 model_file_name = os.path.join('checkpoints', 'to-2019-06-checkpoints', 'weights-improvement-20-0.000289.h5')
 #model_file_name = os.path.join('checkpoints', 'weights-improvement-18-0.000434.h5')
 model_std_error = np.array([0.60684879, 0.57097587, 0.57766695, 0.50046783, 0.50637192, 0.59326133, 0.52038012]) # Inaccuracy based on val data
 model = keras.models.load_model(model_file_name)
 
+
+# Save as spreadsheet
+n_symbols = len(symbols)
+
+input_data = np.zeros((n_symbols, hist_time_steps, 6))	# Shape of input data
+for x in range(n_symbols):
+	input_data[x] = latest_n_df[symbols[x]].values
+
+
+predictions = model.predict(input_data)
+
+# Normalize prediction (to percent change from previous close)
+for sym_i in range(len(predictions)):
+	prev_close = input_data[sym_i][-1][1]
+
+	pred = predictions[sym_i]
+	for i in range(len(pred)):
+		pred[i] = (pred[i] - prev_close) / prev_close * 100	# Percent change
+
+print('{} symbols, {} input rows, {} predictions.'.format(n_symbols, len(input_data), len(predictions)))
+
+
+pred_df = pd.DataFrame(predictions, columns=list(range(len(predictions[0]))))	#rows are symbols, columns are pred values
+
+pred_df_rename_dict = {}
+for sym_i in range(len(symbols)):
+	pred_df_rename_dict[sym_i] = symbols[sym_i]
+pred_df.rename(index=pred_df_rename_dict, inplace=True)
+
+pred_df.to_csv(os.path.join('recommendations', 'date{}-time{}-step{}-recommendations.csv'.format(today, datetime.time(datetime.now()), hist_time_steps)))
+
+
+# Purchase strategy that seems to work
 stocks_to_buy = []
 n_sym = 0
 n_rec = 0
+
 for sym in symbols:
 	n_sym+=1
 	if n_sym%10 == 0:
 		print('Number {} out of {}. {:.1f}% complete'.format(n_sym, len(symbols), n_sym/len(symbols)*100))
-	X = latest_n_df[sym].values
-
-	pred = model.predict(np.array([X]))[0]
+	X = input_data[n_sym-1]
+	
+	pred = predictions[n_sym-1]
 	previous_close = X[-1][1]
-
 
 	max_accuracy_day = np.argmin(model_std_error)
 	if pred[max_accuracy_day] / previous_close > 1.07 and np.mean(pred[1:4]) / previous_close > 1.06:	# Buy conditions
@@ -127,5 +169,8 @@ if input('Preview stocks: ') == 'y':
 
 
 recommendations = " ".join(stocks_to_buy)
-with open(os.path.join('recommendations', '{}-{}-{}step-recommendations.txt'.format(today, datetime.time(datetime.now()), hist_time_steps)), 'w') as f:
+rec_txt_name = os.path.join('recommendations', 'date{}-time{}-step{}-recommendations.txt'.format(today, datetime.time(datetime.now()), hist_time_steps))
+with open(rec_txt_name, 'w') as f:
 	f.write(recommendations)
+
+
