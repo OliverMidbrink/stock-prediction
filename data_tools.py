1a954ba0f40deae8448ee18598b44173194e1a41
Oliver Midbrink <oliver@Olivers-MBP.lan>
2020-03-08 12:26:50 +0100

    New dataset_generator create_sliding_hdf5() seems to work. Tested with 22 GB of data.

--- data_tools.py
+++ data_tools.py
@@ -1,10 +1,13 @@
 from tensorflow.python.keras.utils.data_utils import Sequence
 import numpy as np
 import random
-import os, time, sys
+import os, time, sys, h5py, sklearn
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+
+
+# Use: https://stackoverflow.com/questions/25655588/incremental-writes-to-hdf5-with-h5py?rq=1
 
 '''
 def scalar_augment(X_, min_scalar=0.5, max_scalar=2):
@@ -62,24 +65,22 @@ class CustomSequence(Sequence):
 
 def trim_zeros(X_, Y_):
 	idx = len(X_)
+	print('Before cut: {}.'.format(idx))
 	searching = True
-	
-	X_out = []
-	Y_out = []
 	
 	while searching:
 		idx += -1
 		if np.count_nonzero(X_[idx]) > 0 and np.count_nonzero(Y_[idx]) > 0:
 			print('End Found at Index: {}.'.format(idx))
-			X_out = X_[:idx+1]
-			Y_out = Y_[:idx+1]
+			X_ = X_[:idx+1]
+			Y_ = Y_[:idx+1]
 			searching = False
 
 		if idx == 0:
 			print('Dataset seems to only contain zeros')
 			searching = False
 
-	return X_out, Y_out
+	print('After cut: {}.'.format(len(X_)))
 
 
 
@@ -111,7 +112,7 @@ print('Y avg max: {}.'.format(explore_df['Sum Y'].max()))
 	# 260 market days - around a year
 
 
-def create_sliding_hdf5(output_filename, raw_dataset, hist_time_steps=500, stride=20, pred_time_steps=[1, 3, 5, 10, 20, 65]): #Create a sliding training dataset. Latest time period will be added to the validation set.  
+def create_sliding_hdf5(output_filename, raw_dataset, hist_time_steps=500, stride=30, pred_time_steps=[1, 3, 5, 10, 20, 65]): #Create a sliding training dataset. Latest time period will be added to the validation set.  
 	start_time = time.time()																								  #network therefore starts training at t_now - longest_pred_time_step - stride (could be 85 market days after specified time if 65 pred and 20 stride).
 
 	print('Reading data file...')
@@ -155,6 +156,27 @@ def create_sliding_hdf5(output_filename, raw_dataset, hist_time_steps=500, strid
 	Y_val = np.zeros((max_length, len(pred_time_steps)))
 	Y_test = np.zeros((max_length, len(pred_time_steps)))
 
+	chunk_size = 1000
+
+	hf = h5py.File(output_filename, 'w')
+	X_train = hf.create_dataset('X_train', (0, hist_time_steps, 6), maxshape=(None, hist_time_steps, 6), dtype='float64', chunks=(chunk_size, hist_time_steps, 6))
+	Y_train = hf.create_dataset('Y_train', (0, len(pred_time_steps)), maxshape=(None, len(pred_time_steps)), dtype='float64', chunks=(chunk_size, len(pred_time_steps)))
+
+	X_val = hf.create_dataset('X_val', (0, hist_time_steps, 6), maxshape=(None, hist_time_steps, 6), dtype='float64', chunks=(chunk_size, hist_time_steps, 6))
+	Y_val = hf.create_dataset('Y_val', (0, len(pred_time_steps)), maxshape=(None, len(pred_time_steps)), dtype='float64', chunks=(chunk_size, len(pred_time_steps)))
+
+	X_test = hf.create_dataset('X_test', (0, hist_time_steps, 6), maxshape=(None, hist_time_steps, 6), dtype='float64', chunks=(chunk_size, hist_time_steps, 6))
+	Y_test = hf.create_dataset('Y_test', (0, len(pred_time_steps)), maxshape=(None, len(pred_time_steps)), dtype='float64', chunks=(chunk_size, len(pred_time_steps)))
+
+	batch_x_train = []
+	batch_y_train = []
+
+	batch_x_val = []
+	batch_y_val = []
+
+	batch_x_test = []
+	batch_y_test = []
+
 	split_pattern = [0]	# Train: 0, val: 1, test: 2
 	
 	n_periods = int(	(len(n_df) - hist_time_steps - np.amax(pred_time_steps) - 1) / stride	) + 1
@@ -175,10 +197,6 @@ def create_sliding_hdf5(output_filename, raw_dataset, hist_time_steps=500, strid
 	n_y_contain_zero = 0
 	n_elements = 0
 	n_flat = 0
-
-	train_next_idx = 0
-	val_next_idx = 0
-	test_next_idx = 0
 
 	period_idx = 0
 	for t in range(len(n_df) - hist_time_steps - np.amax(pred_time_steps) - 1, 0, -stride): # Iterate through time sections of the full dataset
@@ -253,45 +271,85 @@ def create_sliding_hdf5(output_filename, raw_dataset, hist_time_steps=500, strid
 				continue
 
 			if period_split == 0:	# Add to traing set
-				X_train[train_next_idx] = x_p
-				Y_train[train_next_idx] = y_p
-				train_next_idx+=1
+				batch_x_train.append(x_p)
+				batch_y_train.append(y_p)
+
+				if len(batch_x_train) == chunk_size:
+					X_train.resize(X_train.shape[0] + chunk_size, axis=0)
+					Y_train.resize(Y_train.shape[0] + chunk_size, axis=0)
+
+					X_train[-chunk_size:] = np.array(batch_x_train)
+					Y_train[-chunk_size:] = np.array(batch_y_train)
+
+					batch_x_train = []
+					batch_y_train = []
+
+					print(X_train.shape)
+
 
 			if period_split == 1:	# Add to validation set
-				X_val[val_next_idx] = x_p
-				Y_val[val_next_idx] = y_p
-				val_next_idx+=1
+				batch_x_val.append(x_p)
+				batch_y_val.append(y_p)
+
+				if len(batch_x_val) == chunk_size:
+					X_val.resize(X_val.shape[0] + chunk_size, axis=0)
+					Y_val.resize(Y_val.shape[0] + chunk_size, axis=0)
+
+					X_val[-chunk_size:] = np.array(batch_x_val)
+					Y_val[-chunk_size:] = np.array(batch_y_val)
+
+					batch_x_val = []
+					batch_y_val = []
+
+					print(X_val.shape)
 
 			if period_split == 2:	# Add to validation set
-				X_test[test_next_idx] = x_p
-				Y_test[test_next_idx] = y_p
-				test_next_idx+=1
+				batch_x_test.append(x_p)
+				batch_y_test.append(y_p)
+
+				if len(batch_x_test) == chunk_size:
+					X_test.resize(X_test.shape[0] + chunk_size, axis=0)
+					Y_test.resize(Y_test.shape[0] + chunk_size, axis=0)
+
+					X_test[-chunk_size:] = np.array(batch_x_test)
+					Y_test[-chunk_size:] = np.array(batch_y_test)
+
+					batch_x_test = []
+					batch_y_test = []
+
+					print(X_test.shape)
 
 		print('Running average. Non finite X, Y: {}, {}. Elements that contained too many zeros: {}, {}. Flat {}'.format(
 			(n_x_non_finite - per_n_x_non_finite) / (n_elements - per_n_elements), (n_y_non_finite - per_n_y_non_finite) / (n_elements - per_n_elements), 
 			(n_x_contain_zero - per_n_x_contain_zero) / (n_elements - per_n_elements), (n_y_contain_zero - per_n_y_contain_zero) / (n_elements - per_n_elements),
 			(n_flat - per_n_flat) / (n_elements - per_n_elements) ))
-		print('{:0.4f}% of periods completed. \n\nETA {:0.2f} minutes\n'.format(period_idx/n_periods * 100, (time.time()-start_time) / 60 / (period_idx/n_periods)))
+		print('{:0.4f}% of periods completed. \n\nETA: {:0.2f} minutes. Elapsed: {}. Iterated: {}\n'.format(period_idx/n_periods * 100, (time.time()-start_time) / 60 / (period_idx/n_periods), (time.time()-start_time) / 60, n_elements))
 
 	print('Filtered. Non finite X, Y: {}, {}. Elements that contained too many zeros: {}, {}. Flat {}'.format(n_x_non_finite / n_elements, n_y_non_finite / n_elements, n_x_contain_zero / n_elements, n_y_contain_zero / n_elements, n_flat / n_elements))
 	
-	X_train, Y_train = trim_zeros(X_train, Y_train)
-	X_val, Y_val = trim_zeros(X_val, Y_val)
-	X_test, Y_test = trim_zeros(X_test, Y_test)
 
-	X_train, Y_train = shuffle(X_train, Y_train)
-	X_val, Y_val = shuffle(X_val, Y_val)
-	X_test, Y_test = shuffle(X_test, Y_test)
+	if len(batch_x_train) > 0:	# if there are elements not added to dataset, add them
+		X_train.resize(X_train.shape[0] + len(batch_x_train), axis=0)
+		Y_train.resize(Y_train.shape[0] + len(batch_y_train), axis=0)
+		
+		X_train[-len(batch_x_train):] = np.array(batch_x_train)
+		Y_train[-len(batch_y_train):] = np.array(batch_y_train)
 
-	hf = h5py.File(output_filename, 'w')
-	hf.create_dataset('X_train', data=X_train)
-	hf.create_dataset('Y_train', data=Y_train)
+	if len(batch_x_val) > 0:	# if there are elements not added to dataset, add them
+		X_val.resize(X_val.shape[0] + len(batch_x_val), axis=0)
+		Y_val.resize(Y_val.shape[0] + len(batch_y_val), axis=0)
+		
+		X_val[-len(batch_x_val):] = np.array(batch_x_val)
+		Y_val[-len(batch_y_val):] = np.array(batch_y_val)
 
-	hf.create_dataset('X_val', data=X_val)
-	hf.create_dataset('Y_val', data=Y_val)
+	if len(batch_x_test) > 0:	# if there are elements not added to dataset, add them
+		X_test.resize(X_test.shape[0] + len(batch_x_test), axis=0)
+		Y_test.resize(Y_test.shape[0] + len(batch_y_test), axis=0)
+		
+		X_test[-len(batch_x_test):] = np.array(batch_x_test)
+		Y_test[-len(batch_y_test):] = np.array(batch_y_test)
 
-	hf.create_dataset('X_test', data=X_test)
-	hf.create_dataset('Y_test', data=Y_test)
+	# Shuffle when generating batches for training instead
 
 	hf.close()
 
